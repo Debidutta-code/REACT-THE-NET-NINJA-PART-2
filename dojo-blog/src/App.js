60be0c031104c2f99ca9761b340feeffa3bfd79c
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2023-06-06 23:35:50 +0530

    changing color and content

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 
 function App() {
+  const place = " paradise";
+  const yourMom = 23;
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>Welcome To {place} and your age is {yourMom}</h1>
       </div>
     </div>
   );

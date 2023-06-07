22a4df8b9844bee053d83e5c1651d58ff82627c4
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2023-06-07 10:10:34 +0530

    4th commit

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 function App() {
   const place = " paradise";
   const yourMom = 23;
+  const googleLink = "www.google.com";
   return (
     <div className="App">
       <div className="content">
         <h1>Welcome To {place} and your age is {yourMom}</h1>
+        <p>{ 10 }</p>
+        <h2>{ "This is dev" }</h2>
+        <p> {[1, 2, 3, 4, 5]} </p>
+        <h1>Your random number after refresh is : {parseInt(Math.random() * 10)}</h1>
+        <a href={googleLink}>Google.com</a>
       </div>
     </div>
   );

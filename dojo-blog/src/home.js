cb8ab16b3452d2291673312c68a624e397153f02
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2023-06-07 20:17:46 +0530

    8th push

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
-    let name = "mario";
+    let prevName = "sinny"
+    let prevAge = 19;
+    const [name, setNewName] = useState(prevName);
+    const [age, setAge] = useState(prevAge);
 
     const handleClick = () => {
-        name = "dev";
+        setNewName('dev');
+        setAge(20);
     }
     return (
         <div className="home">
             <h2>Home page</h2>
             <p>{ name }</p>
+            <p>{ age }</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
